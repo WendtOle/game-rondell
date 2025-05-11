e15b54537b9b644063c82a9e3b8c6ca33d4c69d1
Ole <ole@Mac.fritz.box>
2025-05-11 20:04:53 +0200

    Do not allow votes after session is finished

--- src/SessionView.tsx
+++ src/SessionView.tsx
@@ -3,11 +3,14 @@ import { VoteCreationForm } from "./VoteCreationForm";
 import { VoteList } from "./VoteList";
 import { ResultList } from "./ResultList";
 import { SessionList } from "./SessionList";
+import { useRecoilValue } from "recoil";
+import { sessionState } from "./state/sessions";
 
 export const SessionView = () => {
+  const session = useRecoilValue(sessionState)
   return (
     <div className="mx-auto p-4 w-4xl">
-      <VoteCreationForm />
+      {!session?.finished && <VoteCreationForm />}
       <VoteList />
       <ResultList />
       <SessionList />
