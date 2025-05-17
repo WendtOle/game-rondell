11642c63e32a73912b5f0b961830ceeb38ffbe15
Ole <ole@Mac.fritz.box>
2025-05-17 13:11:09 +0200

    Disable session switching
    Simplifies user interface.
    The logic for handling different sessions is kept

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,38 @@ import React from "react";
 import { VoteCreationForm } from "./VoteCreationForm";
 import { VoteList } from "./VoteList";
 import { ResultList } from "./ResultList";
-import { SessionList } from "./SessionList";
-import { useRecoilValue } from "recoil";
-import { sessionState } from "./state/sessions";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  sessionState,
+  sessionsState,
+  activeSessionIdState,
+} from "./state/sessions";
+import { generateId } from "./utils/generateId";
+import { Session } from "./types";
+import { Button } from "./components/Button";
 
 export const App = () => {
   const session = useRecoilValue(sessionState);
+  const setSessionId = useSetRecoilState(activeSessionIdState);
+  const setSessions = useSetRecoilState(sessionsState);
+
+  const handleClick = () => {
+    const newSession: Session = {
+      id: generateId(),
+      createdAt: Date.now(),
+      votes: [],
+      finished: false,
+    };
+    setSessions((cur) => ({ ...cur, [newSession.id]: newSession }));
+    setSessionId(newSession.id);
+    return;
+  };
   return (
     <div className="mx-auto p-4 w-4xl">
       {!session?.finished && <VoteCreationForm />}
       <VoteList />
       <ResultList />
-      <SessionList />
+      <Button title="Neue Abstimmung starten" onClick={handleClick} />
     </div>
   );
 };
